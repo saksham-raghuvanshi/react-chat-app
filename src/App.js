f976ef8122e2056f040316cdf8d116749e06da6d
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-21 18:53:38 +0530

    public and Private Route Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import "rsuite/dist/rsuite.min.css";
+import "rsuite/dist/styles/rsuite-default.css";
 import "./Style/main.scss";
+import { Switch } from "react-router";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import Privateroute from "./Components/Privateroute";
+import Publicroute from "./Components/Publicroute";
 function App() {
   return (
     <div className="App">
-      <h1>Chat APP</h1>
+      <Switch>
+        {/* <route path="/signin">
+          <SignIn />
+        </route> */}
+
+        <Publicroute path="/signin">
+          <SignIn />
+        </Publicroute>
+        <Privateroute>
+          <Home />
+        </Privateroute>
+      </Switch>
     </div>
   );
 }
