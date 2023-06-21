defee0f2f0c983db49ac6457c08f13f716e058eb
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-21 18:18:33 +0530

    Firebase intialize

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyBSD1gd-pN78NaAYl-oNYFNxvy6bF-eKYk",
+  authDomain: "chat-web-app-2438.firebaseapp.com",
+  projectId: "chat-web-app-2438",
+  storageBucket: "chat-web-app-2438.appspot.com",
+  messagingSenderId: "686928272648",
+  appId: "1:686928272648:web:ca7bcd45640eda4a1320bd",
+};
+
+const app = firebase.initializeApp(config);
