f56a37f0b53b120e73fca4c85cfe14a1f548d76d
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-22 20:56:49 +0530

    Preloader Added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import "rsuite/dist/styles/rsuite-default.css";
 import "./Style/main.scss";
+import "./Style/preloader.scss";
 import { Switch } from "react-router";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import Privateroute from "./Components/Privateroute";
 import Publicroute from "./Components/Publicroute";
+import Preloader from "./Components/Preloader";
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        {/* <route path="/signin">
+    <>
+      <Preloader />
+      <div className="App">
+        <Switch>
+          {/* <route path="/signin">
           <SignIn />
         </route> */}
 
-        <Publicroute path="/signin">
-          <SignIn />
-        </Publicroute>
-        <Privateroute>
-          <Home />
-        </Privateroute>
-      </Switch>
-    </div>
+          <Publicroute path="/signin">
+            <SignIn />
+          </Publicroute>
+          <Privateroute>
+            <Home />
+          </Privateroute>
+        </Switch>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Preloader.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { preLoaderAnim } from "../Animation";
+
+const Preloader = () => {
+  useEffect(() => {
+    preLoaderAnim();
+  }, []);
+  return (
+    <div className="preloader">
+      <div className="preloader-container">
+        <span>Welcome,</span>
+        <span>to</span>
+        <span>Chat</span>
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
