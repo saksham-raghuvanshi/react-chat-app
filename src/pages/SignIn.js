27bef1ea81780148f7a5836641e4a75eb31ad3d5
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-22 20:27:34 +0530

    Sigin page create

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Button, Col, Container, Grid, Icon, Row } from "rsuite";
 
 const SignIn = () => {
-  return <div>Sign In</div>;
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <div className="text-center">
+              <h1>Welcome to Chat</h1>
+              <p>Progessive chat platform For Everyone</p>
+            </div>
+            <div className="mt-3">
+              <Button block color="blue">
+                <Icon icon="facebook"> Continue with Facebook</Icon>
+              </Button>
+
+              <Button block color="green">
+                <Icon icon="google"> Continue with Google</Icon>
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
