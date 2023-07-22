aaf8e1156010bf412fbe3a53acf46832aeb9ab56
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-22 20:21:39 +0530

    Chat List

---
+++ src/Components/chat-window/messages/MessageItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MessageItem = () => {
+  return <div>Hello</div>;
+};
+
+export default MessageItem;
--- src/Components/chat-window/messages/index.js
+++ src/Components/chat-window/messages/index.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router";
+import { database } from "../../../misc/firebase";
+import { transformToArrWithid } from "../../../misc/helper";
+import MessageItem from "./MessageItem";
 
 const Message = () => {
-  return <div>Messages</div>;
+  const { chatId } = useParams();
+  const [messages, setMessages] = useState(null);
+
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messageRef = database.ref("messages");
+    messageRef
+      .orderByChild("roomId")
+      .equalTo("chatId")
+      .on("value", (snap) => {
+        const data = transformToArrWithid(snap.val());
+
+        setMessages(data);
+      });
+
+    return () => {
+      messageRef.off("value");
+    };
+  }, [chatId]);
+
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No Messages Yet</li>}
+      {canShowMessages &&
+        messages.map((msg) => <MessageItem key={msg.id} messages={msg} />)}
+    </ul>
+  );
 };
 
 export default Message;
